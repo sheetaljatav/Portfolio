eebbe544c1c4b758a587f7f0ac33af24bb354307
sheetal jatav <98652006+sheetaljatav@users.noreply.github.com>
2024-12-19 13:17:33 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
   return <section className= {styles.container} id= "about">
     <h2 className= {styles.title}>About</h2>
     <div className= {styles.content}>
-        <img src="public/assets/about/aboutImage.png" alt="Me sitting with a laptop" className={styles.aboutImage}/>
+        <img src="assets/about/aboutImage.png" alt="Me sitting with a laptop" className={styles.aboutImage}/>
     <ul className= {styles.aboutItems}>
         <li className= {styles.aboutItem}>
             <img src ="assets/about/cursorIcon.png" alt="Cursor icon" />
